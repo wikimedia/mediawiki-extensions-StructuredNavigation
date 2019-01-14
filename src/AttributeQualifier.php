wikimedia/mediawiki-extensions-StructuredNavigation

commit 358aef0790a85b77ff006daf0a0981423a9255d4

Hygiene: Reduce slight code duplication in AttributeQualifier
There was originally a tiny bit of code duplication in
\StructuredNavigation\AttributeQualifier for escaping
the content of an attribute - it's now a private helper
method.

src/AttributeQualifier.php
@@ -27,13 +27,21 @@ class AttributeQualifier {
 	 */
 	public function setAttributes( Tag $renderedNavigation, TitleValue $title, array $attributes ) : void {
 		$renderedNavigation->setAttributes( [
-			self::ATTR_DATA_NAME => htmlspecialchars( $title->getText(), ENT_QUOTES )
+			self::ATTR_DATA_NAME => $this->escapeAttributeContent( $title->getText() )
 		] );
 
 		if ( isset( $attributes['id'] ) ) {
 			$renderedNavigation->setAttributes( [
-				'id' => htmlspecialchars( $attributes['id'], ENT_QUOTES )
+				'id' => $this->escapeAttributeContent( $attribute['id'] )
 			] );
 		}
+	}
+
+	/**
+	 * @param string $attribute
+	 * @return string
+	 */
+	private function escapeAttributeContent( string $attribute ) : string {
+		return htmlspecialchars( $attribute, ENT_QUOTES );
 	}
 }

commit d298b186e770680f8b968ec13b4b3a844139c1a2

AttributeQualifier: Fix variable typo introduced from 358aef0
This was my bad; I was testing this on a page that was using
the parser tag but wasn't using the id attribute. This is
fixed now.

src/AttributeQualifier.php
@@ -32,7 +32,7 @@ final class AttributeQualifier {
 
 		if ( isset( $attributes['id'] ) ) {
 			$renderedNavigation->setAttributes( [
-				'id' => $this->escapeAttributeContent( $attribute['id'] )
+				'id' => $this->escapeAttributeContent( $attributes['id'] )
 			] );
 		}
 	}