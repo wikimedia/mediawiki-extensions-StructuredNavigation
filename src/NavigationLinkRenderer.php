wikimedia/mediawiki-extensions-StructuredNavigation

commit 2e8506e9aec48e45849715ddff13785de58ade42

Hygiene: Encapsulate link functionality in separate service
Link functionality is now encapsulated in it's own service
to keep code a bit cleaner and allow for potential reuse.
Maybe this service could be expanded for allowing links for
groups and titles?

New service is at \StructuredNavigation\NavigationLinkRenderer.

src/NavigationLinkRenderer.php
@@ -1,0 +1,51 @@
+<?php
+
+namespace StructuredNavigation;
+
+use MediaWiki\Linker\LinkRenderer;
+use TitleParser;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+final class NavigationLinkRenderer {
+
+	/** @var LinkRenderer */
+	private $linkRenderer;
+
+	/** @var TitleParser */
+	private $titleParser;
+
+	/**
+	 * @param LinkRenderer $linkRenderer
+	 * @param TitleParser $titleParser
+	 */
+	public function __construct(
+		LinkRenderer $linkRenderer,
+		TitleParser $titleParser
+	) {
+		$this->linkRenderer = $linkRenderer;
+		$this->titleParser = $titleParser;
+	}
+
+	/**
+	 * @param array|string $contentTitle
+	 * @param array $attributes
+	 * @return string
+	 */
+	public function getLink( $contentTitle, array $attributes = [] ) : string {
+		$context = [];
+
+		if ( is_array( $contentTitle ) ) {
+			$context = [ 'title' => $contentTitle[0], 'label' => $contentTitle[1] ];
+		} else {
+			$title = $this->titleParser->parseTitle( $contentTitle );
+			$context = [ 'title' => $title, 'label' => $title->getText() ];
+		}
+
+		return $this->linkRenderer->makeLink(
+			$context['title'], $context['label'], $attributes
+		);
+	}
+
+}

src/Renderer/NavigationRenderer.php
@@ -2,14 +2,12 @@
 
 namespace StructuredNavigation\Renderer;
 
-use MediaWiki\Linker\LinkRenderer;
 use OOUI\HtmlSnippet;
 use OOUI\Tag;
+use StructuredNavigation\NavigationLinkRenderer;
 use StructuredNavigation\Json\JsonEntity;
 use StructuredNavigation\Libs\OOUI\Element\DescriptionList;
 use StructuredNavigation\Libs\OOUI\Element\UnorderedList;
-use TitleParser;
-use TitleValue;
 
 /**
  * Renders a structured navigation view. This by default does
@@ -22,11 +20,8 @@ use TitleValue;
  */
 final class NavigationRenderer {
 
-	/** @var LinkRenderer */
-	private $linkRenderer;
-
-	/** @var TitleParser */
-	private $titleParser;
+	/** @var NavigationLinkRenderer */
+	private $navigationLinkRenderer;
 
 	/**
 	 * @var array Conveniently keeps all CSS class keys in one place.
@@ -45,15 +40,10 @@ final class NavigationRenderer {
 	];
 
 	/**
-	 * @param LinkRenderer $linkRenderer
-	 * @param TitleParser $titleParser
+	 * @param NavigationLinkRenderer $navigationLinkRenderer
 	 */
-	public function __construct(
-		LinkRenderer $linkRenderer,
-		TitleParser $titleParser
-	) {
-		$this->linkRenderer = $linkRenderer;
-		$this->titleParser = $titleParser;
+	public function __construct( NavigationLinkRenderer $navigationLinkRenderer ) {
+		$this->navigationLinkRenderer = $navigationLinkRenderer;
 	}
 
 	/**
@@ -118,19 +108,13 @@ final class NavigationRenderer {
 		$allContent = [];
 
 		foreach ( $content as $contentItem ) {
-			$html = '';
-
-			if ( is_array( $contentItem ) ) {
-				$html = $this->doRenderContentItem(
-					$this->titleParser->parseTitle( $contentItem[0] ),
-					$contentItem[1]
-				);
-			} else {
-				$title = $this->titleParser->parseTitle( $contentItem );
-				$html = $this->doRenderContentItem( $title, $title->getText() );
-			}
-
-			array_push( $allContent, $html );
+			array_push(
+				$allContent,
+				$this->navigationLinkRenderer->getLink(
+					$contentItem,
+					[ 'class' => $this->cssClasses['group-content-link'] ]
+				)
+			);
 		}
 
 		return new UnorderedList( [
@@ -138,17 +122,5 @@ final class NavigationRenderer {
 			'item-attributes' => [ 'class' => $this->cssClasses['group-content-item'] ],
 			'classes' => [ $this->cssClasses['group-content-list'] ]
 		] );
-	}
-
-	/**
-	 * @param TitleValue $title
-	 * @param string $text
-	 * @return string
-	 */
-	private function doRenderContentItem( TitleValue $title, string $text ) : string {
-		return $this->linkRenderer->makeLink(
-			$title, $text,
-			[ 'class' => $this->cssClasses['group-content-link'] ]
-		);
 	}
 }

src/Services/Constants.php
@@ -14,6 +14,9 @@ final class Constants {
 	public const SERVICE_CONFIG = 'StructuredNavigation.Config';
 
 	/** @var string */
+	public const SERVICE_NAVIGATION_LINK_RENDERER = 'StructuredNavigation.NavigationLinkRenderer';
+
+	/** @var string */
 	public const SERVICE_NAVIGATION_RENDERER = 'StructuredNavigation.NavigationRenderer';
 
 	/** @var string */

src/Services/ServiceWiring.php
@@ -5,6 +5,7 @@ namespace StructuredNavigation\Services;
 use Config;
 use MediaWiki\MediaWikiServices;
 use StructuredNavigation\AttributeQualifier;
+use StructuredNavigation\NavigationLinkRenderer;
 use StructuredNavigation\Hooks\ParserFirstCallInitHandler;
 use StructuredNavigation\Renderer\NavigationRenderer;
 
@@ -17,10 +18,16 @@ return [
 		return $services->getConfigFactory()->makeConfig( Constants::CONFIG_NAME );
 	},
 
-	Constants::SERVICE_NAVIGATION_RENDERER => function ( MediaWikiServices $services ) : NavigationRenderer {
-		return new NavigationRenderer(
+	Constants::SERVICE_NAVIGATION_LINK_RENDERER => function ( MediaWikiServices $services ) : NavigationLinkRenderer {
+		return new NavigationLinkRenderer(
 			$services->getLinkRenderer(),
 			$services->getTitleParser()
+		);
+	},
+
+	Constants::SERVICE_NAVIGATION_RENDERER => function ( MediaWikiServices $services ) : NavigationRenderer {
+		return new NavigationRenderer(
+			$services->getService( Constants::SERVICE_NAVIGATION_LINK_RENDERER )
 		);
 	},
 

src/Services/Services.php
@@ -5,6 +5,7 @@ namespace StructuredNavigation\Services;
 use Config;
 use ConfigException;
 use MediaWiki\MediaWikiServices;
+use StructuredNavigation\NavigationLinkRenderer;
 use StructuredNavigation\Hooks\ParserFirstCallInitHandler;
 use StructuredNavigation\Renderer\NavigationRenderer;
 use UnexpectedValueException;
@@ -42,6 +43,13 @@ final class Services {
 	 */
 	public function getConfig() : Config {
 		return $this->services->getService( Constants::SERVICE_CONFIG );
+	}
+
+	/**
+	 * @return NavigationLinkRenderer
+	 */
+	public function getNavigationLinkRenderer() : NavigationLinkRenderer {
+		return $this->services->getService( Constants::SERVICE_NAVIGATION_LINK_RENDERER );
 	}
 
 	/**

commit 9f613ce51c1629cce1dec2a405fe14cbafd747ee

Bugfix: Properly handle rendering links that are passed as arrays
Section: Explanation of schema
There are two ways defined by the schema to create a link for a
navigation (e.g title is Set:70778):
 1. as a string: "Set:70778"
      The renderer will pass the string and parse it into a TitleValue
      object, then use it to create that as the page to link to, with
      the label being the same as the page.
 2. as an array: [ "Set:70778", "Protector of Jungle" ]
      The renderer will parse the array and pass Set:70778 as the page
      to link to, but it will show "Protector of Jungle" as the label
      of the link.

Section: Why did this happen?
 - tl;dr The issue was caused by 2e8506e, which no longer tried to use the
   TitleParser service to create a new TitleValue object if the given
   data was an array. This has now been fixed.
 - Testing was done weakly and improperly:
   This was not caught because this was overlooked while testing;
   this was tested with the 'wikipedia-en-dontnod-entertainment.json'
   sample, which only contains string-formatted based links (defined by
   schema), but was not tested with other navigations like
   'biosector01-bionicle-sets-2016.json`, which has all of its links
   using the schema-defined array format. Future patches will be better
   tested against so something like this doesn't happen again.

Thank you again for using this extension!

src/NavigationLinkRenderer.php
@@ -29,23 +29,20 @@ final class NavigationLinkRenderer {
 	}
 
 	/**
-	 * @param array|string $contentTitle
+	 * @param string|array $contentTitle
 	 * @param array $attributes
 	 * @return string
 	 */
 	public function getLink( $contentTitle, array $attributes = [] ) : string {
-		$context = [];
-
 		if ( is_array( $contentTitle ) ) {
-			$context = [ 'title' => $contentTitle[0], 'label' => $contentTitle[1] ];
+			$parsedTitle = $this->titleParser->parseTitle( $contentTitle[0] );
+			$label = $contentTitle[1];
 		} else {
-			$title = $this->titleParser->parseTitle( $contentTitle );
-			$context = [ 'title' => $title, 'label' => $title->getText() ];
+			$parsedTitle = $this->titleParser->parseTitle( $contentTitle );
+			$label = $parsedTitle->getText();
 		}
 
-		return $this->linkRenderer->makeLink(
-			$context['title'], $context['label'], $attributes
-		);
+		return $this->linkRenderer->makeLink( $parsedTitle, $label, $attributes );
 	}
 
 }