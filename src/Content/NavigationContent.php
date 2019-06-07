43bcee93dbff3549cf2bf44131254fa1620f2cd9
Samantha Nguyen <samanthanguyen1116@gmail.com>
2019-06-06 23:33:13 -0500

    show actual navigation when viewing in the Navigation namespace
    Next step is to reduce code duplication. Tracked in #62

--- src/Content/NavigationContent.php
+++ src/Content/NavigationContent.php
@@ -2,7 +2,14 @@
 
 namespace StructuredNavigation\Content;
 
+use FormatJson;
 use JsonContent;
+use Title;
+use OutputPage;
+use ParserOptions;
+use ParserOutput;
+use StructuredNavigation\Services\Services;
+use StructuredNavigation\Json\JsonEntity;
 
 /**
  * Content object for representing a structured navigation.
@@ -18,4 +25,27 @@ final class NavigationContent extends JsonContent {
 		parent::__construct( $text, CONTENT_MODEL_NAVIGATION );
 	}
 
+	/**
+	 * @inheritDoc
+	 */
+	protected function fillParserOutput(
+		Title $title,
+		$revId,
+		ParserOptions $options,
+		$generateHtml,
+		ParserOutput &$output
+	) {
+		if ( $generateHtml ) {
+			$navigationView = Services::getInstance()->getNavigationView();
+			$jsonEntity = new JsonEntity( FormatJson::decode( $this->getText(), true ) );
+
+			OutputPage::setupOOUI();
+			$output->addModuleStyles( [
+				'ext.structurednavigation.ui.structurednavigation.styles',
+				'ext.structurednavigation.ui.structurednavigation.separator.styles'
+			] );
+
+			$output->setText( $navigationView->getView( $jsonEntity ) );
+		}
+	}
 }
